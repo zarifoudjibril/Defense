19b68676a6057a0e1cf35e70b41d38ffd3b0f1c9
zarifoudjibril <67414124+zarifoudjibril@users.noreply.github.com>
2021-11-24 21:52:36 +0000

    Upload all files

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById("dat").innerHTML = Date();
+});
+
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  document.getElementById("dat1").innerHTML = Date();
+});
